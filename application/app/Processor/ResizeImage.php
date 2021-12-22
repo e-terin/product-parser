27fad9fbefb6aa75091de05e95432c528d45261a
Evgeniy Terin <web@derin.ru>
2021-12-22 23:16:42 +0300

    [I-2] dont work :(

---
+++ application/app/Processor/ResizeImage.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Processor;
+
+use Imagick;
+use ImagickPixel;
+
+/**
+ * Проверяет кол-во картинок в коллекции Product
+ */
+class ResizeImage extends AbstractProcessor
+{
+    private const OUTPUT_FORMAT = 'jpg';
+
+    /**
+     * @throws \ImagickException
+     */
+    public function process()
+    {
+        $products = $this->params->getCollection();
+
+        // TODO: check directory
+
+        foreach ($products as $product_id=>$product){
+            foreach ($this->settings->images as $image){
+                $a= $image;
+                if (!empty($product->images[$image])) {
+                    $img = new Imagick($product->images[$image]);
+                    $width = $img->getImageWidth();
+                    $height = $img->getImageHeight();
+                    $new_width = $this->settings['output']['width'];
+                    $new_height = $new_width  * $height / $width ;
+                    $img->resizeImage($new_width, $new_height, Imagick::FILTER_UNDEFINED, 1, 1, 1);
+                    $img->setImageFormat($this->settings['output']['format']);
+
+                    $output_image_dir =
+                        $_ENV['DIR_WORK']
+                        . 'out/'
+                        . $this->scenario_settings['category']
+                        . '/'
+                        . $image
+                        . '/';
+
+                    if (!is_dir($output_image_dir) && !mkdir($output_image_dir) && !is_dir($output_image_dir)) {
+                        throw new \RuntimeException(sprintf('Directory "%s" was not created', $output_image_dir));
+                    }
+
+                    $output_image_name = $output_image_dir
+                         . $product_id
+                         . '.'
+                         . self::OUTPUT_FORMAT;
+
+                    $img->writeImage($output_image_name);
+
+                    if ($this->scenario_settings['test'] ?? false) {
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        $this->output = $this->params;
+    }
+}
